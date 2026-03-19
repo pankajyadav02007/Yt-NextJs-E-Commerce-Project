71521b7d3bbc69ddc0cc5f25bdc13aec401ce046
pankaj <pankajyadav221306@gmail.com>
2026-03-19 13:41:23 +0530

    checkbox, mediaApi, orderNotifications logic concepts

--- email/otpEmail.js
+++ email/otpEmail.js
@@ -125,7 +125,7 @@ export const otpEmail = (otp) => {
 																	style="line-height:10px">
 																	<div style="max-width: 250px;"><img
 																			alt="reset-password" height="auto"
-																			src="https://res.cloudinary.com/do7xdfl3y/image/upload/v1737487850/next-ecommerce/rb_27348_hfzgxd.png"
+																			src="https://res.cloudinary.com/dmicfxing/image/upload/v1773898669/otp-email_xukb5h.png"
 																			style="display: block; height: auto; border: 0; width: 100%;"
 																			title="reset-password" width="250" /></div>
 																</div>
